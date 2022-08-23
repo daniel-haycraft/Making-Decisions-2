a29c0ef286dba7c526f1698a2169e514a52c603e
Daniel Haycraft <111464698+daniel-haycraft@users.noreply.github.com>
2022-08-23 18:35:10 +0000

    practice

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 let array = [7, 12, -4, "hello"]
-for (let i = 0; i < array.length; i++)
-  
+// for (let i = 0; i < array.length; i++){
+//   console.log(array[i])
+// }
+
+for (let i = array.length-1; i >= 0; i--){
+  console.log(array[i])
+}
